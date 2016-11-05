2e5d517954b966174e581be5caffb7f4908d555b
pcduck111 <champ_ped@hotmail.com>
2016-11-05 11:39:17 +0700

    first commit

---
+++ app/src/main/java/com/example/champ/myclass/Detail_viewActivity.java
@@ -0,0 +1,13 @@
+package com.example.champ.myclass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Detail_viewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_view);
+    }
+}
